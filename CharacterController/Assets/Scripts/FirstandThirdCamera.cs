368740187f167ff9262371b1b0a95a068e744b34
RICHARD-PC\RICHARD <ta3agustinruizrojo@gmail.com>
2018-12-19 22:42:47 +0100

    001

---
+++ CharacterController/Assets/Scripts/FirstandThirdCamera.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstandThirdCamera : MonoBehaviour {
+
+    public GameObject FirtsPerson;
+    public GameObject ThirdPerson;
+    public int CamMode;
+
+	// Use this for initialization
+	void Start ()
+    {
+               
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButtonDown("Camera"))
+        {
+            if (CamMode == 1)
+            {
+                CamMode = 0;
+            }
+
+            else
+            {
+                CamMode += 1;
+            }
+            StartCoroutine(CamChange());
+        }
+
+    }
+
+    IEnumerator CamChange()
+    {
+        yield return new WaitForSeconds(0.01f);
+
+        if (CamMode == 0)
+        {
+            ThirdPerson.SetActive(true);
+            FirtsPerson.SetActive(false);
+        }
+
+        if (CamMode == 1)
+        {
+            ThirdPerson.SetActive(false);
+            FirtsPerson.SetActive(true);
+        }
+
+    }
+}
